68c8003232e4f560ce698488fb62c269c353daa9
leticiabrt <britotpleticia@gmail.com>
2023-11-17 10:41:47 -0300

    quase la

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Home as PageHome } from "./Home"
 export { Estante as PageList } from "./List"
 export { Layout as PageLayout } from "./Layout"
+export { default as AdmPage } from "./Adm"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom"
-import { PageHome, PageList, PageLayout } from "../pages"
-
-export function RouteSoftware() {
+import { AdmPage, CadastrarPage, PageHome, PageList, PageLayout, LoginPage } from "../pages"
+import ProtectedRoute from "./ProtectedRoute"
+const Rotas = () => {
   return (
     <Routes>
-      <Route path="/" element={<PageLayout />}>
+      <Route element={<PageLayout />}>
         <Route path="" element={<PageHome />} />
+        <Route path="/cadastrar" element={<CadastrarPage />}/> 
+        <Route path="//login" element={<LoginPage />}/>
+        <Route element={<ProtectedRoute />} />
+          <Route path="/adm" element={<AdmPage />} />
         <Route path="/list" element={<PageList />} />
       </Route>
     </Routes>
   )
+
 }
+export default Rotas
+  
